201c60f08ee81de05cde610695b7a22a24f928a7
MathisL971 <63963669+MathisL971@users.noreply.github.com>
2023-05-14 23:36:53 -0400

    complete exercises 3.1-3.6

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
-const persons = [
+let persons = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -27,6 +28,7 @@ const persons = [
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log("http://localhost:3001/api/persons");
 });
 
 // Get all contacts
@@ -36,9 +38,65 @@ app.get("/api/persons", (req, res) => {
 
 // Get info about contacts
 app.get("/info", (req, res) => {
+  const d = new Date();
   res.send(
     `<p>Phonebook has info for ${
       persons.length
-    } people</p><br /><p>${Date.now()}</p>`
+    } people</p><p>${d.toISOString()}</p>`
   );
+});
+
+// Get a specific contact
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((p) => id === p.id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+// Delete a specific contact
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((p) => p.id !== id);
+  res.status(204).end();
+});
+
+const generateId = () => {
+  let maxId = 0;
+  persons.forEach((p) => {
+    if (maxId < p.id) {
+      maxId = p.id;
+    }
+  });
+  return maxId + 1;
+};
+
+// Create a contact
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  if ([...persons.map((p) => p.name)].includes(body.name)) {
+    return res.status(400).json({
+      error: "name already exists",
+    });
+  }
+
+  const contact = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(contact);
+  res.json(contact);
 });

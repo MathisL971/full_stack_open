a5e2a27644863889e1e3bd0b33ea44f333c18244
MathisL971 <63963669+MathisL971@users.noreply.github.com>
2023-05-19 19:58:25 -0400

    working on 3.14

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -36,9 +36,18 @@ app.get("/api/notes", (request, response) => {
 
 // Get a specific note
 app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(400).send({ error: "malformatted id" });
+    });
 });
 
 // Create a note
@@ -62,10 +71,28 @@ app.post("/api/notes", (request, response) => {
 });
 
 // Delete a note
-app.delete("/api/notes/:id", (request, response) => {
-  Note.findByIdAndDelete(request.params.id).then((result) => {
-    console.log(result);
-  });
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+// Toggle importance of a note
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.use(unknownEndpoint);
--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -14,18 +14,14 @@ const requestLogger = (req, res, next) => {
   next();
 };
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
 });
 
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 app.use(requestLogger);
-app.use(express.static("build"));
 
 // Get all contacts
 app.get("/api/persons", (req, res) => {
@@ -46,20 +42,21 @@ app.get("/info", (req, res) => {
 });
 
 // Get a specific contact
-app.get("/api/persons/:id", (req, res) => {
-  Contact.findById(req.params.id).then((result) => {
-    res.json(result);
-  });
+app.get("/api/persons/:id", (req, res, next) => {
+  Contact.findById(req.params.id)
+    .then((contact) => {
+      if (contact) {
+        res.json(result);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
-// Delete a specific contact
-app.delete("/api/persons/:id", (req, res) => {
-  Contact.findByIdAndDelete(req.params.id).then((result) => {
-    console.log(result);
-  });
-});
-
-// Create a contact
+// Create a contact or update an existing contact
 app.post("/api/persons", (req, res) => {
   const body = req.body;
 
@@ -69,23 +66,70 @@ app.post("/api/persons", (req, res) => {
     });
   }
 
-  // if ([...persons.map((p) => p.name)].includes(body.name)) {
-  //   return res.status(400).json({
-  //     error: "name already exists",
-  //   });
-  // }
+  Contact.find({ name: body.name }).then((existingContact) => {
+    if (existingContact.length != 0) {
+      console.log("Contact already exists!");
+      app.put("/api/persons/:id", (req, res, next) => {
+        console.log("Making put request!");
+        const contact = {
+          name: body.name,
+          number: body.number,
+        };
 
-  const contact = new Contact({
-    name: body.name,
-    number: body.number,
-  });
+        Contact.findByIdAndUpdate(existingContact[0].id, contact, {
+          new: true,
+        })
+          .then((result) => {
+            console.log("Contact updated!");
+            res.json(result);
+          })
+          .catch((error) => {
+            next(error);
+          });
+      });
+    } else {
+      console.log("This is a new contact!");
+      const contact = new Contact({
+        name: body.name,
+        number: body.number,
+      });
 
-  contact.save().then((savedContact) => {
-    res.json(savedContact);
+      contact.save().then((savedContact) => {
+        console.log("Saving new contact!");
+        res.json(savedContact);
+      });
+    }
   });
 });
 
+// Delete a specific contact
+app.delete("/api/persons/:id", (req, res, next) => {
+  Contact.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {

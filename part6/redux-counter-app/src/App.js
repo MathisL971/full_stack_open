d2e2b46cedd72501c2648f99ded24d1ddb2dbe73
MathisL971 <63963669+MathisL971@users.noreply.github.com>
2023-06-18 18:45:21 -0500

    start part6

---
+++ part6/redux-counter-app/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <span>0</span>
+      <br></br>
+      <button>Plus</button>
+      <button>Minus</button>
+      <button>Zero</button>
+    </div>
+  );
+}
+
+export default App;
